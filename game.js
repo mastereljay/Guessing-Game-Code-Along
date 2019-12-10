4f3a67cb0d1b975492a545fe72228a1e5cd7bcd9
mastereljay <normanlj77@hotmail.com>
2019-12-10 12:25:23 -0800

    created the script, but its not working correctly, the if statement is not displaying the alert when guessed correctly.

--- game.js
+++ game.js
@@ -1,3 +1,10 @@
-// Make sure things are connected okay
+// Create secretNumber
+var secretNumber = 4;
 
-alert("HELLO!");
+// Ask user for guess
+var guess = prompt("Guess a number");
+
+// Check if guess is right
+if(guess === secretNumber) {
+	alert("YOU GOT IT RIGHT!");
+}
