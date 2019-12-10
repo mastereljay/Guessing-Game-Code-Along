4e5493a926b4d12ed29a38f70e2edbc515086e18
mastereljay <normanlj77@hotmail.com>
2019-12-10 12:53:09 -0800

    my crack at a higher guess with an alert

--- game.js
+++ game.js
@@ -5,12 +5,20 @@ var secretNumber = 4;
 var guess = prompt("Guess a number");
 
 // Check if guess is right
-// Added Number() here so that the guess and therefore the secretNumber is identified as a number, not a string
 if(Number(guess) === secretNumber) {
   alert("YOU GOT IT RIGHT!");
 }
 
-// Otherwise, you got it wrong
+// Otherwise, check if higher
 else {
-	alert("Wrong!")
+	if(Number(guess) > secretNumber) {
+		alert("NOPE, LOWER!")
+	}
+}
+
+// Otherwise, check if lower
+else {
+	if(Number(guess) < secretNumber) {
+		alert("NOPE, HIGHER!")
+	}
 }
